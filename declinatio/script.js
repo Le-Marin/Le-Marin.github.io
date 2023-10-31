496c5140999e85051cc489a65516fb0f3b645f58
Le-Marin <119621011+Le-Marin@users.noreply.github.com>
2023-10-31 06:04:50 +0300

    Update

--- declinatio/script.js
+++ declinatio/script.js
@@ -1,17 +1,16 @@
 (function() {
   'use strict';
 
-  const sort = (array) => array.sort(() => Math.random() - 0.5);
-  const vocabulary = sort(VOCABULARY.splice(0));
-  vocabulary.forEach((that) => that.unshift(false));
-
   [...document.scripts].forEach((el) => el.remove());
 
   const $ = (selector, ctx = document) => ctx.querySelector(selector);
   const $$ = (selector, ctx = document) => [...ctx.querySelectorAll(selector)];
+  const sort = (array) => array.sort(() => Math.random() - 0.5);
+
+  const vocabulary = sort(VOCABULARY.splice(0));
 
   const select = $('select');
-  const selectLen = select.options.length;
+  const selectLen = select.options.length - 1;
   const btnGet = $('.get');
   const btnCheck = $('.check');
   const btnAnswers = btnCheck.nextElementSibling;
@@ -20,12 +19,13 @@
   const table2 = table.cloneNode(true);
   const inputs = $$('.input', table);
   const inputs2 = $$('.input', table2);
-  const fields = [inputs.slice(0, 6), inputs.slice(6)];
+  const qnt = inputs.length / 2;
+  const fields = [inputs.slice(0, qnt), inputs.slice(qnt)];
 
   let currentWord = null;
   let isAnswersShown = false;
 
-  const getInflection = (ind) => currentWord[4 + ind / 6 >> 0][ind % 6] || '';
+  const getInflection = (ind) => currentWord[3 + ind / qnt >> 0][ind % qnt] || '';
 
   table2.classList.add('result');
   inputs2.forEach((el, i) => {
@@ -35,17 +35,14 @@
     inputs2[i] = div;
   });
 
-  btnGet.addEventListener('click', function get() {
-    const declension = +select.value || 1 + Math.random() * selectLen >> 0;
-    const test = (that) => that[1] === declension;
-    currentWord = vocabulary.find(data => !data[0] && test(data));
-
-    if (!currentWord) {
-      if (+select.value) return rearrangeDeclension(test, get);
-      return handleRandomNull(get);
+  btnGet.addEventListener('click', function() {
+    if (currentWord) {
+      vocabulary.push(vocabulary.splice(vocabulary.indexOf(currentWord), 1)[0]);
     }
 
-    currentWord[0] = true;
+    const declension = +select.value || 1 + Math.random() * selectLen >> 0;
+    currentWord = vocabulary.find(([d]) => d === declension);
+
     fillWords();
 
     inputs.forEach((el) => {
@@ -53,41 +50,20 @@
       el.parentNode.removeAttribute('data-valid');
     });
 
-    currentWord[4].forEach((x, i) => clearValue(fields[0][i]));
-    currentWord[5].forEach((x, i) => clearValue(fields[1][i]));
+    currentWord[3].forEach((x, i) => clearValue(fields[0][i]));
+    currentWord[4].forEach((x, i) => clearValue(fields[1][i]));
 
-    table2.classList.toggle('no-vocative', currentWord[4].length < 6);
+    table2.classList.toggle('no-vocative', currentWord[3].length < qnt);
     table2.remove();
     isAnswersShown = false;
 
     if (btnAnswers.textContent[0] !== 'R') btnAnswers.textContent = 'Mónstráre';
   });
 
-  function handleRandomNull(callback) {
-    if (vocabulary.find(data => !data[0])) return callback();
-
-    sort(vocabulary).forEach((data) => data[0] = false);
-    callback();
-  }
-
-  function rearrangeDeclension(predicate, callback) {
-    const extracts = [];
-
-    [...vocabulary].forEach((data, i) => {
-      if (!predicate(data)) return;
-
-      data[0] = false;
-      vocabulary.splice(i - extracts.push(data) + 1, 1);
-    });
-
-    vocabulary.push(...sort(extracts));
-    callback();
-  }
-
   function fillWords() {
     const reGen = / (f|m|n|pl|mf|mfn)$/;
-    words[0].innerHTML = currentWord[2].replace(reGen, ' <b>$1</b>');
-    words[1].innerHTML = currentWord[3];
+    words[0].innerHTML = currentWord[1].replace(reGen, ' <b>$1</b>');
+    words[1].innerHTML = currentWord[2];
   }
 
   function setNoValue(el) {
@@ -102,14 +78,59 @@
     el.parentNode.removeAttribute('data-none');
   }
 
+  // =========================
+
+  const btnCase = $('.case');
+  const cases = {'ā': 'a', 'ē': 'e', 'ī': 'i', 'ō': 'o', 'ū': 'u', 'ȳ': 'y'};
+
+  btnCase.addEventListener('click', () => btnCase.classList.toggle('__disabled'));
+
   btnCheck.addEventListener('click', function() {
     if (!currentWord) return;
 
+    const isCaseSensitive = !btnCase.classList.contains('__disabled');
+
     inputs.forEach((el, i) => {
+      const {dataset} = el.parentNode;
+
+      if (+dataset.none) return;
+
       const userValue = el.value.trim().toLowerCase();
+
+      if (!userValue) return dataset.valid = 0;
+
       const value = getInflection(i).replaceAll('_', '').toLowerCase();
-      const isValid = value.split('/').some(x => x === userValue);
-      el.parentNode.dataset.valid = +isValid;
+      const chunks = value.split('/');
+
+      if (chunks.length === 1 && value.length !== userValue.length) {
+        return dataset.valid = 0;
+      }
+
+      if (chunks.length > 1 && value.includes(' ')) {
+        const key = +!chunks[0].includes(' ');
+        const res = [chunks[key].split(' ')[key], chunks[1 - key]];
+        chunks[1 - key] = (key ? res.reverse() : res).join(' ');
+      }
+
+      const isValid = chunks.some(x => x === userValue);
+
+      if (isValid || isCaseSensitive) return dataset.valid = +isValid;
+
+      dataset.valid = +chunks.some(value => {
+        for (let i = 0; i < value.length; i++) {
+          let a = value[i];
+          let b = userValue[i];
+
+          if (a in cases) {
+            a = cases[a];
+            b = cases[b] || b;
+          }
+
+          if (a !== b) return;
+        }
+
+        return true;
+      });
     });
   });
 
@@ -130,6 +151,8 @@
     this.textContent = 'Céláre';
     isAnswersShown = true;
   });
+
+  // =========================
 
   const keyboard = (function() {
     const target = $('.keyboard');
